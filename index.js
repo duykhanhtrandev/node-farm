94736898e56891a97b5a33867b4aafbf16360e9c
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2023-03-02 11:11:41 +0700

    filling the templates

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ const url = require('url');
 
 /////////////////////////////
 // SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -39,6 +42,9 @@ const server = http.createServer((req, res) => {
     res.end('This is the OVERVIEW');
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT');
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
@@ -49,5 +55,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, '127.0.0.1', () => {
-  console.log('Listning to requests on port 8000');
+  console.log('Listning to requests on port http://localhost:8000');
 });
