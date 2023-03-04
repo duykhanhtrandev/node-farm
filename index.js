33ec5494b8b72c3467cfe279719b0cb0ed99dab7
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2023-03-04 22:04:32 +0700

    3rd party modules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 /////////////////////////////
@@ -34,7 +34,6 @@ const replaceTemplate = require('./modules/replaceTemplate');
 
 /////////////////////////////
 // SERVER
-
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8'
@@ -50,6 +49,8 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
