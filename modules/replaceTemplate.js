c68d694a1361d7e2505100e8d3f58da56e7a1139
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2023-03-04 23:40:06 +0700

    vnd currency formatting

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -1,7 +1,13 @@
 module.exports = (temp, product) => {
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
   output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(
+    /{%PRICE%}/g,
+    parseInt(product.price).toLocaleString('vi', {
+      style: 'currency',
+      currency: 'VND',
+    })
+  );
   output = output.replace(/{%FROM%}/g, product.from);
   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
